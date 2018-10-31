dfa9f7e68ee2586f76a6b3540fe4b5e8a269b9f8
Игорь Жарников <urapywka@gmail.com>
2018-10-31 22:34:44 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import PortalVue from 'portal-vue'
 
 Vue.config.productionTip = false
+
+import Button from './components/Button/Button'
+Vue.component("Button", Button)
+
+Vue.use(PortalVue)
 
 new Vue({
   render: h => h(App)
